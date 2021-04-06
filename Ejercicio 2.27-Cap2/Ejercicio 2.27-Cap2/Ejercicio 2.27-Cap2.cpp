70f646f200b54505f6b9000469e4614b479e1a68
Bryan <bry@DESKTOP-MGIN8VI>
2021-04-06 00:07:25 -0500

    Ejercicio 2.27

---
+++ Ejercicio 2.27-Cap2/Ejercicio 2.27-Cap2/Ejercicio 2.27-Cap2.cpp
@@ -0,0 +1,18 @@
+// Ejercicio 2.27-Cap2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //EJERCICIO 2.27
+    char l;
+    int num;
+    cout << "EJERCICIO 2.27" << endl;
+    cout << "Ingrese un caracter: ";
+    cin >> l;
+    num = l;
+    cout << "Su caracter en valor entero es: " << num;
+    cout << endl;
+    cout << endl;
+}
