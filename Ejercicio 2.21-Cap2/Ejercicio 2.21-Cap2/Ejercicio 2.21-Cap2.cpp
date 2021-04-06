dd50fe3fc6b9a9480167fe0b597e6888b7bf73b5
Bryan <bry@DESKTOP-MGIN8VI>
2021-04-06 00:02:24 -0500

    Ejercicio 2.21

---
+++ Ejercicio 2.21-Cap2/Ejercicio 2.21-Cap2/Ejercicio 2.21-Cap2.cpp
@@ -0,0 +1,20 @@
+// Ejercicio 2.21-Cap2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //EJERCICIO 2.21
+    cout << "EJERCICIO 2.21" << endl;
+    cout << "\tccc" << "\t +" << "\t   +" << endl;
+    cout << "       c" << "\t +" << "\t   +" << endl;
+    cout << "      c" << "\t       +++++" << "\t +++++" << endl;
+    cout << "       c" << "\t +" << "\t   +" << endl;
+    cout << "\tccc" << "\t +" << "\t   +" << endl;
+
+    cout << endl;
+}
+
+
