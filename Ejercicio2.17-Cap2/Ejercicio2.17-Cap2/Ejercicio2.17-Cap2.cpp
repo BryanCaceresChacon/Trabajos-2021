b1fc25d1afd8a4fcd8c44c11c8347ee2f7419450
Bryan <bry@DESKTOP-MGIN8VI>
2021-04-05 23:59:08 -0500

    Ejercicio 2.17

---
+++ Ejercicio2.17-Cap2/Ejercicio2.17-Cap2/Ejercicio2.17-Cap2.cpp
@@ -0,0 +1,28 @@
+// Ejercicio2.17-Cap2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //EJERCICIO 2.17:
+    cout << "EJERCICIO 2.17" << endl;
+    string v;
+    string x;
+    string y;
+    string z;
+    v = "v";
+    x = "x";
+    y = "y";
+    z = "z";
+    //A)
+    cout << v + x << "," << y + z << endl;
+    //b)
+    cout << v + x + y + z << "," << z + y + x + v << "," << x + y + z + v << "," << y + v + x + z << endl;
+    //c)
+    cout << v + x + y + z;
+    cout << endl;
+    cout << endl;
+}
+
